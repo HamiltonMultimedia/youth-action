164efc3ace0d3730765c0632f1d4a502a764925f
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2019-08-17 17:09:36 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from '../src/images/ya-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo} className="" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The <code>youth action</code> website will be active here.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.fridaysforfuture.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Associated With
         </a>
       </header>
     </div>
