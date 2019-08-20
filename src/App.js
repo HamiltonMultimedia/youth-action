854600f7ec7f51f1678afa58a3f04c5fc641822c
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2019-08-20 09:54:28 -0400

    Added Vimeo Player

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src={logo} className="" alt="logo" />
+        <img src={logo} className="" alt="logo" style={{width:'56%'}}/>
         <p>
           The <code>youth action</code> website will be active here.
         </p>
@@ -20,6 +21,8 @@ function App() {
           Associated With
         </a>
       </header>
+      <div style={{padding: '56.25% 0 0 0', position: 'relative'}}><iframe src="https://player.vimeo.com/video/354777368?autoplay=1&title=0&byline=0&portrait=0" style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}} frameBorder={0} allow="autoplay; fullscreen" allowFullScreen /></div>
+        <p>If The Video Is Not Playing <a href="https://vimeo.com/354777368">Click Here</a>!</p><p />
     </div>
   );
 }
